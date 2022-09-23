794187d156a7d1bf081951dd92b959e62a64c778
Stian Aannerud <47632822+Stia4@users.noreply.github.com>
2022-09-23 15:12:26 +0200

    Added C and modified B

---
+++ Project1c.py
@@ -0,0 +1,89 @@
+from Project1b import *
+from sklearn.utils import resample
+from tqdm import trange
+
+### Make coarse data with high order polynomial fits
+nx, ny = (10, 10)
+orders = [i for i in range(8)]
+
+x, y, z = make_Franke(nx, ny, noise = 0.1)
+x_l, y_l, z_l, x_t, y_t, z_t = train_test_data_evenly(x, y, z, n=4) # l = learn, t = test
+X_l, X_t, beta, z_lm, z_tm = fit_OLS_SVD(orders, x_l, y_l, z_l, x_t, y_t, z_t) # m = model
+
+### Recreate Fig 2.11 of Hastie, Tibshirani, and Friedman
+figname = lambda name: f"fig/{name}_{min(orders)}-{max(orders)}_{len(x)}x{len(y)}.pdf"
+MSE_lrn = [MSE(z_l, z_lm[i]) for i in orders]
+MSE_tst = [MSE(z_t, z_tm[i]) for i in orders]
+plt.plot(orders, MSE_lrn, "o-", label="MSE Training data")
+plt.plot(orders, MSE_tst, "o-", label="MSE Test data")
+plt.hlines(0, min(orders), max(orders), colors="gray", linestyles="dashed")
+plt.xticks(ticks=orders)
+plt.xlabel("Polynome order")
+plt.ylabel("Mean squared error")
+plt.legend(loc=1)
+plt.grid(alpha = 0.3)
+plt.tight_layout()
+#plt.savefig(figname("MSE_2.11"), format="pdf")
+plt.show()
+plt.clf()
+
+### Bias-variance analysis, based on code https://compphysics.github.io/MachineLearning/doc/LectureNotes/_build/html/chapter3.html#the-bias-variance-tradeoff
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.utils import resample
+
+#from Project1b import FrankeFunction, create_X
+np.random.seed(0)
+
+n = 625
+n_boostraps = 100
+maxdegree = 18
+
+# Make data set.
+x = np.arange(0, 1, 1.0/np.ceil(n**0.5))
+y = np.arange(0, 1, 1.0/np.ceil(n**0.5))
+x_, y_ = np.meshgrid(x, y)
+z = FrankeFunction(x_, y_)
+z += np.random.normal(0, np.mean(z)*0.1, z.shape) # Adding some normal noise
+z -= np.mean(z) # Centering the data
+
+x = np.append(x_.ravel().reshape(-1, 1), y_.ravel().reshape(-1, 1), axis=1)
+y = z.ravel()
+y = y.reshape(y.size, 1)
+
+error = np.zeros(maxdegree)
+bias = np.zeros(maxdegree)
+variance = np.zeros(maxdegree)
+polydegree = np.zeros(maxdegree)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+for degree in range(maxdegree):
+    #model = make_pipeline(LinearRegression(fit_intercept=False))
+    model = LinearRegression(fit_intercept=False)
+    y_pred = np.empty((y_test.shape[0], n_boostraps))
+    X_t = create_X(x_test[:, 0], x_test[:, 1], n=degree)
+    for i in range(n_boostraps):
+        x_, y_ = resample(x_train, y_train)
+        X = create_X(x_[:, 0], x_[:, 1], n=degree)
+        y_pred[:, i] = model.fit(X, y_).predict(X_t).ravel()
+
+    polydegree[degree] = degree
+    error[degree] = np.mean( np.mean((y_test - y_pred)**2, axis=1, keepdims=True) )
+    bias[degree] = np.mean( (y_test - np.mean(y_pred, axis=1, keepdims=True))**2 )
+    variance[degree] = np.mean( np.var(y_pred, axis=1, keepdims=True) )
+    print('Polynomial degree:', degree)
+    print('Error:', error[degree])
+    print('Bias^2:', bias[degree])
+    print('Var:', variance[degree])
+    print('{} >= {} + {} = {}'.format(error[degree], bias[degree], variance[degree], bias[degree]+variance[degree]))
+
+plt.plot(polydegree, error, label='Error')
+plt.plot(polydegree, bias, label='bias')
+plt.plot(polydegree, variance, label='Variance')
+plt.legend()
+plt.yscale('log')
+plt.show()
