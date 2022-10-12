8fba1e47897b5031dd6c38303597caf04097525a
Stian Aannerud <47632822+Stia4@users.noreply.github.com>
2022-10-12 13:39:58 +0200

    Cleaning

--- Project1f.py
+++ Project1f.py
@@ -40,19 +40,15 @@ def Bootstrap_pred(x_l, x_t, y_l, y_t, z_l, z_t, n_bootstraps, maxdegree, lam=0.
             z_pred[degree, :, i] = lasso_model.fit(data_learn, z_lr).predict(data_test)
     return z_pred
 
-### Same main part as from E, only function has changed
 def main_1f():
     nx, ny = (10, 10)                 # number of Franke points to generate in x,y
     n_degrees = 10                    # number of degrees, degrees 0 ... n_degrees-1
     orders = np.linspace(0, n_degrees-1, n_degrees)
-    nk = 4                            # number of k-folds
     n_bootstraps = 100               # number of times to resample each learn-set
-    # lambdas = np.logspace(-10, -1, 3)  # values of lambda to use in Ridge regression
     lambdas = lambdas = [1e-10, 1e-5, 1e-1]  # values of lambda to use in Ridge regression
     lambdas_str = [r"10$^{-10}$", r"10$^{-5}$", r"10$^{-1}$"]
 
     x, y, z = make_Franke(nx, ny, noise=0.1, seed=0)
-    # learn_set, test_set = kfold(x, y, z, nk, seed=0)
 
     # Bootstrap
     test_size = 4
@@ -62,10 +58,6 @@ def main_1f():
         bias     = np.zeros(n_degrees)
         variance = np.zeros(n_degrees)
         error    = np.zeros(n_degrees)
-        # for k in trange(nk):
-        #     x_lk, y_lk, z_lk = learn_set[:, k]
-        #     x_tk, y_tk, z_tk =  test_set[:, k]
-        # z_pred = Bootstrap_pred(x_lk, x_tk, y_lk, y_tk, z_lk, z_tk, n_bootstraps, n_degrees, lam)
         z_pred = Bootstrap_pred(x_lrn, x_tst, y_lrn, y_tst, z_lrn, z_tst, n_bootstraps, n_degrees, lam)
         for degree in range(n_degrees):
             error[degree]    += np.mean( np.mean((z_tst[:, np.newaxis] - z_pred[degree])**2, axis=1, keepdims=True) )
@@ -76,14 +68,10 @@ def main_1f():
         plt.plot(np.arange(n_degrees), bias,     color=f"C{i}", linestyle="--")
         plt.plot(np.arange(n_degrees), variance, color=f"C{i}", linestyle="-.")
 
-    figname = lambda name: f"img_f/{name}_Lasso_{min(orders)}-{max(orders)}_{nx}x{ny}.pdf"
+    figname = lambda name: f"fig/{name}_Lasso_{min(orders)}-{max(orders)}_{nx}x{ny}.pdf"
 
-    # plt.plot([], [], color="k", linestyle="-",  label="Error")
-    # plt.plot([], [], color="k", linestyle="--", label="Bias")
-    # plt.plot([], [], color="k", linestyle="-.", label="Variance")
     plt.title("Bias-variance tradeoff for Lasso with Bootstrap")
     plt.xlabel("Polynomial order")
-    # plt.ylabel("Mean squared error")
     plt.yscale('log')
     figure = plt.gcf()
     figure.set_size_inches(8, 6)
