84558159401cc7489018b24815a9896818c26bf5
Stian Aannerud <47632822+Stia4@users.noreply.github.com>
2022-11-18 17:54:33 +0100

    Moved Project 1 into folder

--- project1/Project1e.py
+++ project1/Project1e.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn.utils import resample
 import matplotlib.pyplot as plt
 from tqdm import trange
+plt.rcParams.update({'font.size':16})
 
 def fit_Ridge(orders, lam, x_lrn, y_lrn, z_lrn, x_tst=None, y_tst=None, z_tst=None):
     """
@@ -19,7 +20,7 @@ def fit_Ridge(orders, lam, x_lrn, y_lrn, z_lrn, x_tst=None, y_tst=None, z_tst=No
         I = np.identity(X_lrn[i].shape[1])
         beta_OLS[i] = SVDinv(X_lrn[i].T @ X_lrn[i] + lam*I) @ X_lrn[i].T @ z_lrn
         z_lrn_model[i] = np.reshape(X_lrn[i] @ beta_OLS[i], z_lrn.shape)
-	
+
 	### If given, apply model to test-set as well
     if (x_tst is not None and y_tst is not None and z_tst is not None):
         X_tst = dict()
@@ -31,35 +32,6 @@ def fit_Ridge(orders, lam, x_lrn, y_lrn, z_lrn, x_tst=None, y_tst=None, z_tst=No
 
     ### If test-set not given, only return learn-set model results
     return X_lrn, beta_OLS, z_lrn_model
-
-# def Bootstrap(x_l, x_t, y_l, y_t, z_l, z_t, n_bootstraps, maxdegree, lam=0.0, silent=False):
-#     """
-#     Code based on:
-#     https://compphysics.github.io/MachineLearning/doc/LectureNotes/_build/html/chapter3.html#the-bias-variance-tradeoff
-#     """
-#     error = np.zeros(maxdegree)
-#     bias = np.zeros(maxdegree)
-#     variance = np.zeros(maxdegree)
-
-#     for degree in range(maxdegree):
-#         X_t = create_X(x_t, y_t, n=degree)
-#         z_pred = np.empty((len(z_t), n_bootstraps))
-#         for i in range(n_bootstraps):
-#             x_lr, y_lr, z_lr = resample(x_l, y_l, z_l, random_state = 0 + degree*n_bootstraps + i)
-#             z_pred[:, i] = X_t @ fit_Ridge([degree], lam, x_lr, y_lr, z_lr)[1][degree]
-
-#         error[degree] = np.mean( np.mean((z_t[:, np.newaxis] - z_pred)**2, axis=1, keepdims=True) )
-#         bias[degree] = np.mean( (z_t - np.mean(z_pred, axis=1, keepdims=True))**2 )
-#         variance[degree] = np.mean( np.var(z_pred, axis=1, keepdims=True) )
-
-#         if not silent:
-#             print('Polynomial degree:', degree)
-#             print('Error:', error[degree])
-#             print('Bias^2:', bias[degree])
-#             print('Var:', variance[degree])
-#             print('{} >= {} + {} = {}'.format(error[degree], bias[degree], variance[degree], bias[degree]+variance[degree]))
-
-#     return bias, variance, error
 
 def Bootstrap_pred(x_l, x_t, y_l, y_t, z_l, z_t, n_bootstraps, maxdegree, lam=0.0):
     """
@@ -73,17 +45,35 @@ def Bootstrap_pred(x_l, x_t, y_l, y_t, z_l, z_t, n_bootstraps, maxdegree, lam=0.
             z_pred[degree, :, i] = X_t @ fit_Ridge([degree], lam, x_lr, y_lr, z_lr)[1][degree]
     return z_pred
 
+from Project1b import make_Franke, train_test_data_evenly, MSE, R2_Score
+from sklearn.metrics import mean_squared_error as MSE_skl
+from sklearn.metrics import r2_score as R2_skl
+
 def main_1e():
+    ### Make goal data
+    nx, ny = (30, 30)
+    x, y, z = make_Franke(nx, ny, noise = 0.1, seed=0)
+
+    ### Split data into training and test data, using ~25% as test data
+    test_size = 4 # Reserves 1/4 of the data as test data
+    x_lrn, y_lrn, z_lrn, x_tst, y_tst, z_tst = train_test_data_evenly(x, y, z, test_size)
+
+    orders = [0, 1, 2, 3, 4, 5]
+
     ### Defining input parameters
     nx, ny = (10, 10)                 # number of Franke points to generate in x,y
-    n_degrees = 20                    # number of degrees, degrees 0 ... n_degrees-1
-    nk = 4                            # number of k-folds
-    n_bootstraps = 100                # number of times to resample each learn-set
-    lambdas = np.logspace(-10, -1, 5) # values of lambda to use in Ridge regression
+    n_degrees = 10                    # number of degrees, degrees 0 ... n_degrees-1
+    orders = np.linspace(0, n_degrees-1, n_degrees)
+    n_bootstraps = 100               # number of times to resample each learn-set
+    lambdas = [1e-10, 1e-5, 1e-1] # values of lambda to use in Ridge regression
+    lambdas_str = [r"10$^{-10}$", r"10$^{-5}$", r"10$^{-1}$"]
 
     ### Making dataset
     x, y, z = make_Franke(nx, ny, noise=0.1, seed=0)
-    learn_set, test_set = kfold(x, y, z, nk, seed=0)
+
+    # Bootstrap
+    test_size = 4
+    x_lrn, y_lrn, z_lrn, x_tst, y_tst, z_tst = train_test_data_evenly(x, y, z, test_size)
 
     ### Looping over lambdas, each generating a set of bias/variance/error curves
     for i, lam in enumerate(lambdas):
@@ -91,31 +81,30 @@ def main_1e():
         variance = np.zeros(n_degrees)
         error    = np.zeros(n_degrees)
 
-        ### Looping over k-folds, using a different combination each time
-        for k in trange(nk):
-            x_lk, y_lk, z_lk = learn_set[:, k] # x,y,z for learn set nr. k
-            x_tk, y_tk, z_tk =  test_set[:, k] # x,y,z for test set
+        # Performing n_bootstraps resamplings of learn set and resulting n_degrees models to each test set
+        z_pred = Bootstrap_pred(x_lrn, x_tst, y_lrn, y_tst, z_lrn, z_tst, n_bootstraps, n_degrees, lam) # <- Note lambda is input here
 
-            # Performing n_bootstraps resamplings of learn set and resulting n_degrees models to each test set
-            z_pred = Bootstrap_pred(x_lk, x_tk, y_lk, y_tk, z_lk, z_tk, n_bootstraps, n_degrees, lam) # <- Note lambda is input here
-
-            # Finding how well each model fits across all bootstraps, doing running average over all k-folds
-            for degree in range(n_degrees): #         Broadcasting ↓   Axis with bootstrapped values ↓
-                error[degree]    += np.mean( np.mean((z_tk[:, np.newaxis] - z_pred[degree])**2, axis=1, keepdims=True) ) / nk # <- Note division by nk to
-                bias[degree]     += np.mean( (z_tk[:, np.newaxis] - np.mean(z_pred[degree], axis=1, keepdims=True))**2 ) / nk #    get a running average
-                variance[degree] += np.mean( np.var(z_pred[degree], axis=1, keepdims=True) ) / nk
+        # Finding how well each model fits across all bootstraps, doing running average over all k-folds
+        for degree in range(n_degrees): #         Broadcasting ↓   Axis with bootstrapped values ↓
+            error[degree]    += np.mean( np.mean((z_tst[:, np.newaxis] - z_pred[degree])**2, axis=1, keepdims=True) )
+            bias[degree]     += np.mean( (z_tst[:, np.newaxis] - np.mean(z_pred[degree], axis=1, keepdims=True))**2 )
+            variance[degree] += np.mean( np.var(z_pred[degree], axis=1, keepdims=True) ) / nk
 
         ### Plotting curves for current lambda, same colour for same lambda but different linestyles
-        plt.plot(np.arange(n_degrees), error,    color=f"C{i}", linestyle="-", label=fr"$\lambda =$ {lam}")
+        plt.plot(np.arange(n_degrees), error,    color=f"C{i}", linestyle="-", label=r"$\lambda =$ {}".format(lambdas_str[i]))
         plt.plot(np.arange(n_degrees), bias,     color=f"C{i}", linestyle="--")
         plt.plot(np.arange(n_degrees), variance, color=f"C{i}", linestyle="-.")
 
+    figname = lambda name: f"fig/{name}_Ridge_{min(orders)}-{max(orders)}_{nx}x{ny}.pdf"
+
     ### Formatting and showing plot
-    plt.plot([], [], color="k", linestyle="-",  label="Error") # Linestyle labels to distinguish error/bias/variance
-    plt.plot([], [], color="k", linestyle="--", label="Bias")
-    plt.plot([], [], color="k", linestyle="-.", label="Variance")
+    plt.title("Bias-variance tradeoff for Ridge with Bootstrap")
+    plt.xlabel("Polynomial order")
     plt.yscale('log')
-    plt.legend()
+    figure = plt.gcf()
+    figure.set_size_inches(8, 6)
+    plt.legend(loc='best', bbox_to_anchor=(0.37, 0.22, 0., 0.3))
+    plt.savefig(figname("bias-variance_tradeoff"), format="pdf")
     plt.show()
 
 if __name__ == "__main__":
--- project1/Project1f.py
+++ project1/Project1f.py
@@ -1,4 +1,4 @@
-from Project1b import create_X, make_Franke
+from Project1b import create_X, make_Franke, train_test_data_evenly
 from Project1d import kfold
 import numpy as np
 from sklearn.utils import resample
@@ -9,6 +9,23 @@ from sklearn.linear_model import Lasso
 from sklearn.utils._testing import ignore_warnings # Ignoring convergence warning, else
 from sklearn.exceptions import ConvergenceWarning  # the console will be filled with
 @ignore_warnings(category=ConvergenceWarning)      # walls of text
+
+def fit_Lasso(degrees, lam, x_l, y_l, z_l, x_t, y_t, z_t):
+    """
+    Modified bootstrap code to extract only prediction matrix
+    """
+    z_pred = np.empty((len(degrees), len(z_t)))
+    lasso_model = Lasso(alpha=lam)
+    for degree in degrees:
+        data_test = create_X(x_t, y_t, n=degree)
+        data_learn = create_X(x_l, y_l, n=degree)
+        z_pred[degree, :] = lasso_model.fit(data_learn, z_l).predict(data_test)
+    return z_pred
+
+from sklearn.utils._testing import ignore_warnings # Ignoring convergence warning, else
+from sklearn.exceptions import ConvergenceWarning  # the console will be filled with
+@ignore_warnings(category=ConvergenceWarning)      # walls of text
+
 def Bootstrap_pred(x_l, x_t, y_l, y_t, z_l, z_t, n_bootstraps, maxdegree, lam=0.0):
     """
     Modified bootstrap code to extract only prediction matrix
@@ -23,36 +40,43 @@ def Bootstrap_pred(x_l, x_t, y_l, y_t, z_l, z_t, n_bootstraps, maxdegree, lam=0.
             z_pred[degree, :, i] = lasso_model.fit(data_learn, z_lr).predict(data_test)
     return z_pred
 
-### Same main part as from E, only function has changed
 def main_1f():
     nx, ny = (10, 10)                 # number of Franke points to generate in x,y
-    n_degrees = 20                    # number of degrees, degrees 0 ... n_degrees-1
-    nk = 4                            # number of k-folds
-    n_bootstraps = 100                # number of times to resample each learn-set
-    lambdas = np.logspace(-5, -1, 5)  # values of lambda to use in Ridge regression
+    n_degrees = 10                    # number of degrees, degrees 0 ... n_degrees-1
+    orders = np.linspace(0, n_degrees-1, n_degrees)
+    n_bootstraps = 100               # number of times to resample each learn-set
+    lambdas = lambdas = [1e-10, 1e-5, 1e-1]  # values of lambda to use in Ridge regression
+    lambdas_str = [r"10$^{-10}$", r"10$^{-5}$", r"10$^{-1}$"]
 
     x, y, z = make_Franke(nx, ny, noise=0.1, seed=0)
-    learn_set, test_set = kfold(x, y, z, nk, seed=0)
+
+    # Bootstrap
+    test_size = 4
+    x_lrn, y_lrn, z_lrn, x_tst, y_tst, z_tst = train_test_data_evenly(x, y, z, test_size)
+
     for i, lam in enumerate(lambdas):
         bias     = np.zeros(n_degrees)
         variance = np.zeros(n_degrees)
         error    = np.zeros(n_degrees)
-        for k in trange(nk):
-            x_lk, y_lk, z_lk = learn_set[:, k]
-            x_tk, y_tk, z_tk =  test_set[:, k]
-            z_pred = Bootstrap_pred(x_lk, x_tk, y_lk, y_tk, z_lk, z_tk, n_bootstraps, n_degrees, lam)
-            for degree in range(n_degrees):
-                error[degree]    += np.mean( np.mean((z_tk[:, np.newaxis] - z_pred[degree])**2, axis=1, keepdims=True) ) / nk
-                bias[degree]     += np.mean( (z_tk[:, np.newaxis] - np.mean(z_pred[degree], axis=1, keepdims=True))**2 ) / nk
-                variance[degree] += np.mean( np.var(z_pred[degree], axis=1, keepdims=True) ) / nk
-        plt.plot(np.arange(n_degrees), error,    color=f"C{i}", linestyle="-", label=fr"$\lambda =$ {lam}")
+        z_pred = Bootstrap_pred(x_lrn, x_tst, y_lrn, y_tst, z_lrn, z_tst, n_bootstraps, n_degrees, lam)
+        for degree in range(n_degrees):
+            error[degree]    += np.mean( np.mean((z_tst[:, np.newaxis] - z_pred[degree])**2, axis=1, keepdims=True) )
+            bias[degree]     += np.mean( (z_tst[:, np.newaxis] - np.mean(z_pred[degree], axis=1, keepdims=True))**2 )
+            variance[degree] += np.mean( np.var(z_pred[degree], axis=1, keepdims=True) ) / nk
+
+        plt.plot(np.arange(n_degrees), error,    color=f"C{i}", linestyle="-", label=r"$\lambda =$ {}".format(lambdas_str[i]))
         plt.plot(np.arange(n_degrees), bias,     color=f"C{i}", linestyle="--")
         plt.plot(np.arange(n_degrees), variance, color=f"C{i}", linestyle="-.")
-    plt.plot([], [], color="k", linestyle="-",  label="Error")
-    plt.plot([], [], color="k", linestyle="--", label="Bias")
-    plt.plot([], [], color="k", linestyle="-.", label="Variance")
+
+    figname = lambda name: f"fig/{name}_Lasso_{min(orders)}-{max(orders)}_{nx}x{ny}.pdf"
+
+    plt.title("Bias-variance tradeoff for Lasso with Bootstrap")
+    plt.xlabel("Polynomial order")
     plt.yscale('log')
-    plt.legend()
+    figure = plt.gcf()
+    figure.set_size_inches(8, 6)
+    plt.legend(loc="center left")
+    plt.savefig(figname("bias-variance_tradeoff"), format="pdf")
     plt.show()
 
 if __name__ == "__main__":
