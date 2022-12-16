22358cbbc8f7f75102a95b21798d8eeaa5cc911d
Stian Aannerud <47632822+Stia4@users.noreply.github.com>
2022-12-16 17:27:27 +0100

    Trained model further and made plots

--- project3/CardClassification.py
+++ project3/CardClassification.py
@@ -42,7 +42,7 @@ def print_score(NN, X_test, t_test, multiclass=False):
 
     return p, cost, acc
 
-def save_plot_with_metadata(NN: NeuralNetwork, filename: str, params, n_train, n_iters):
+def save_plot_with_metadata(NN: NeuralNetwork, filename: str, params, n_train, n_test):
     """
     Allows us to save plots along with information how it was created,
     results in simple way to recreate plots if needed, without bloated blocks of code
@@ -53,7 +53,7 @@ def save_plot_with_metadata(NN: NeuralNetwork, filename: str, params, n_train, n
     NB, potential gradient accumulation is not stored, and is reset with every
     time network is setup anew.
     """
-    others = np.array([seed, params, n_train, n_iters], dtype=object)
+    others = np.array([seed, params, n_train, n_test], dtype=object)
 
     NN.save_network("fig/setups/NN_"+filename+".npy")
     NN.save_params("fig/setups/WB_"+filename+".npy")
@@ -97,40 +97,29 @@ def BinaryTest():
     print("--Initial state--")
     p, cost, acc = print_score(NN, X_test, t_test)
     
-    n_iters = 50
+    n_iters = 10
     p_list = [p]; acc_list = [acc] # Collect state of system while training
-    try:
-        for i in range(n_iters-1):
-            print(f"--Iteration {i+1}--")
-            NN.train(X_train, t_train, epochs=1, seed=seed, silent=False)
-            p, cost, acc = print_score(NN, X_test, t_test)
-            p_list.append(p); acc_list.append(acc)
-    except KeyboardInterrupt:
-        # Note: Partial training step is not discarded with interruption,
-        # network is not accountable/reproducable if used after interrupt,
-        # just here for easy testing
-        n_iters = len(p_list)
+    for i in range(n_iters):
+        print(f"--Iteration {i+1}--")
+        NN.train(X_train, t_train, epochs=1, seed=seed, silent=False)
+        p, cost, acc = print_score(NN, X_test, t_test)
+        p_list.append(p); acc_list.append(acc)
 
     # NN.save_params("params/wb.npy")
 
     ##### Plotting section #####
-    fig, (ax0, ax1) = plt.subplots(2, 1, gridspec_kw={'height_ratios': [3, 1]}, sharex=True)
+    plt.rcParams.update({'font.size': 20})
+    fig, (ax0, ax1) = plt.subplots(2, 1, gridspec_kw={'height_ratios': [3, 1]}, sharex=True, figsize=(12, 8))
     for i, p in enumerate(np.array(p_list).T):
         color = "red" if t_test[i] == 1 else "blue"
-        ax0.plot(np.arange(n_iters), p, color=color)
-    ax1.plot(np.arange(n_iters), acc_list)
+        ax0.plot(np.arange(n_iters+1), p, color=color)
+    ax1.plot(np.arange(n_iters+1), acc_list)
+    plt.suptitle("Per-node output probability and accuracy score evolution")
+    ax0.set_ylabel("Node output probability")
+    ax1.set_ylabel("Accuracy")
+    ax1.set_xlabel("Epochs")
     plt.tight_layout()
-    save_plot_with_metadata(NN, 'test', params, n_train_cards, n_iters)
-    plt.show()
-
-    fig, ax = plt.subplots(*(2, 3))
-    ax = ax.flatten()
-    for i, axi in enumerate(ax):
-        a = NN.layers[i].x[..., 0]
-        axi.imshow(a)
-    fig.set_figheight(10)
-    fig.set_figwidth(15)
-    fig.tight_layout()
+    save_plot_with_metadata(NN, 'OutputEvol', params, n_train_cards, 10)
     plt.show()
 
 ### All 4 suits
@@ -181,8 +170,9 @@ def SuitTest():
 def FullTest():
     ##### Data #####
     params = [{'suit':suit, 'num':rank} for suit in range(1,5) for rank in range(1,14)] # All 52 suit+rank combinations
-    n_train_cards = 10 # per class in params
+    n_train_cards = 20 # per class in params
     n_test_cards = 10
+    # n_test_cards = 1
     X_train, X_test, t_train, t_test = load_data(params, size=(n_train_cards, n_test_cards), fullsize=False)
 
     ##### Network setup #####
@@ -201,31 +191,100 @@ def FullTest():
     NN.set_LearningRate(eta0, method=ada_method)
 
     ##### Training section #####
-    NN.load_params("params/wb52.npy")
+    NN.load_params("params/All52_20C_88Train40+Test.npy")
+    # NN.load_params("params/wb52.npy")
 
     print_data(t_train, t_test)
     print_structure(image_size, NN, ada_method, eta0, costfunc)
     print("--Initial state--")
-    print_score(NN, X_test, t_test, multiclass=True)
+    p, cost, acc = print_score(NN, X_test, t_test, multiclass=True)
 
-    n_iters = 3
-    for i in range(n_iters-1):
+    n_iters = 20
+    n_iters = 0
+    for i in range(n_iters):
         print(f"--Iteration {i+1}--")
         NN.train(X_train, t_train, epochs=1, seed=seed, silent=False)
         print_score(NN, X_train, t_train, multiclass=True)
 
-    NN.save_params("params/wb52.npy")
+    # NN.save_params("params/wb52.npy")
 
     ##### Plotting #####
 
-    # CONFUSTION MATRIX
+    ## CONFUSTION MATRIX
+    from sklearn.metrics import confusion_matrix
+    import seaborn as sns
 
-    # Vis igjen at alt går mot p=1/52
-    # Gjør dette med varianse? Mean vil alltid være 1/52, men variansen (per output på 52 noder) vil synke
-    # Også vis antall kategorier som blir brukt for hele test-settet, dvs. a=[7,7,7,7] -> a=[0,1,34,1] for å vise at det spesialiseres
-    # cost også over tid?
+    suit = {0:'♠', 1:'♥', 2:'♣', 3:'♦'}
+    rank = {0:'A', 1:'2', 2:'3', 3:'4', 4:'5', 5:'6', 6:'7', 7:'8', 8:'9', 9:'10', 10:'J', 11:'Q', 12:'K'}
+
+    a = np.argmax(p, axis=1) # Categorized output
+    conf = confusion_matrix(t_test, a)
+    plt.rcParams.update({'font.size': 20})
+    fig, ax = plt.subplots(figsize = (8, 8))
+    sns.heatmap(conf, annot=False, ax=ax, cmap="viridis", cbar=False)
+    ax.set_xticks(np.arange(0, 52, 13) + 0.5)
+    ax.set_yticks(np.arange(0, 52, 13) + 0.5)
+    xticks = np.floor(ax.get_xticks()).astype(int)
+    yticks = np.floor(ax.get_yticks()).astype(int)
+    ax.set_xticklabels([suit[x // 13] + rank[x % 13] for x in xticks])
+    ax.set_yticklabels([suit[y // 13] + rank[y % 13] for y in yticks])
+    ax.set_title("52 card classification confusion matrix")
+    ax.set_ylabel("True value")
+    ax.set_xlabel("Network output")
+    plt.tight_layout()
+    save_plot_with_metadata(NN, 'ConfusionMatrix', params, n_train_cards, n_test_cards)
+    plt.show()
+
+    ## Feature maps
+    g = lambda x: x / np.max(x)
+    norm = lambda x: g(x - np.min(x))
+
+    xn, yn = NN.layers[0].a.shape[:-1] # feature map shape
+    conc = np.empty((4*xn, 4*yn))
+    for i in range(4):
+        for j in range(4):
+            conc[i*xn:(i+1)*xn, j*yn:(j+1)*yn] = norm(NN.layers[0].a[..., 4*i + j]) # Each picture is normalized individually
+    plt.figure(figsize=(8,8))
+    plt.imshow(conc)
+    plt.axis('off')
+    plt.title("Output of initial convolutional layer")
+    save_plot_with_metadata(NN, '16Maps', params, n_train_cards, n_test_cards)
+    plt.show()
+
+    xn, yn = NN.layers[4].a.shape[:-1] # feature map shape
+    conc = np.empty((8*xn, 8*yn))
+    for i in range(8):
+        for j in range(8):
+            conc[i*xn:(i+1)*xn, j*yn:(j+1)*yn] = norm(NN.layers[4].a[..., 8*i + j])
+    plt.figure(figsize=(8,8))
+    plt.imshow(conc)
+    plt.axis('off')
+    plt.title("Output of final convolutional layer")
+    save_plot_with_metadata(NN, '64Maps', params, n_train_cards, n_test_cards)
+    plt.show()
+
+    ## Plot a correct and a wrong cards
+    yes = np.where(t_test == a)[0] # All correct cards
+    no  = np.where(t_test != a)[0] # All wrong cards
+    n = len(yes)-1
+
+    types = {'spades':1, 'hearts':2, 'clubs':3, 'diamonds':4}
+    numbers = {'ace':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7,
+            'eight':8, 'nine':9, 'ten':10, 'jack':11, 'queen':12, 'king':13}
+    types = {v: k for k, v in types.items()} # Reverse dictionary
+    numbers = {v: k for k, v in numbers.items()}
+
+    fig, ax = plt.subplots(1, 2, figsize=(12,8))
+    ax[0].imshow(X_test[yes[n]], cmap='Greys_r')
+    ax[1].imshow(X_test[ no[n]], cmap='Greys_r')
+    ax[0].set_title(f"This is the {numbers[a[yes[n]] % 13 + 1]} of {types[a[yes[n]] // 13 + 1]}")
+    ax[1].set_title(f"This is the {numbers[a[ no[n]] % 13 + 1]} of {types[a[ no[n]] // 13 + 1]}")
+    ax[0].set_xticks([]); ax[0].set_yticks([])
+    ax[1].set_xticks([]); ax[1].set_yticks([])
+    save_plot_with_metadata(NN, 'GoodBadCards', params, n_train_cards, n_test_cards)
+    plt.show()
 
 if __name__ == "__main__":
-    # BinaryTest()
+    BinaryTest()
     # SuitTest()
     FullTest()
---
+++ project3/raw_data/README.md
@@ -0,0 +1,5 @@
+This is the folder which should contain the raw downloaded data from Kaggle. Top folders should be 'test', 'train', and 'valid', along with card info 'cards.csv' and two h5 files which are not used.
+
+Dataset:
+
+https://www.kaggle.com/datasets/gpiosenka/cards-image-datasetclassification
